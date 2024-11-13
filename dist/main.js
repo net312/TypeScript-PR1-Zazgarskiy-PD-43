00ccbc1fd99b5dbb1105e4b1199c4693b4949aa8
Enot317 <vlad_adamovich@ukr.net>
2024-11-13 19:21:39 +0200

    correcting repository

--- dist/main.js
+++
@@ -1,8 +0,0 @@
-import { openMenu } from './menu';
-const tabLinks = document.querySelectorAll('.tablink');
-tabLinks.forEach(link => {
-    link.onclick = (evt) => {
-        const menuName = link.innerText;
-        openMenu(evt, menuName);
-    };
-});
